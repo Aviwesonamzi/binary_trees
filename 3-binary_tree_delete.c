0012dad66e1f75f1a350c933368186be7cefa907
root <you@example.com>
2024-06-04 16:23:50 -0700

    Binary_trees

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,36 +1,16 @@
 #include <stdlib.h>
-
-/**
- * struct binary_tree_s - Binary tree node
- *
- * @n: Integer stored in the node
- * @left: Pointer to the left child node
- * @right: Pointer to the right child node
- */
-struct binary_tree_s
-{
-    int n;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
-};
-
-typedef struct binary_tree_s binary_tree_t;
+#include "binary_trees.h"
 
 /**
  * binary_tree_delete - Deletes an entire binary tree
- * @tree: A pointer to the root node of the tree to delete
- *
- * Description: If tree is NULL, do nothing. Uses post-order traversal.
+ * @tree: Pointer to the root node of the tree to delete
  */
 void binary_tree_delete(binary_tree_t *tree)
 {
-    if (tree)
-    {
-        // Recursively delete left subtree
-        binary_tree_delete(tree->left);
-        // Recursively delete right subtree
-        binary_tree_delete(tree->right);
-        // Delete current node
-        free(tree);
-    }
+    if (tree == NULL)
+        return;
+
+    binary_tree_delete(tree->left);
+    binary_tree_delete(tree->right);
+    free(tree);
 }
