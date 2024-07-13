fc98029e94a7631832a260f2393559e48fb23e63
root <you@example.com>
2024-07-13 06:25:26 -0700

    size

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,22 +1,16 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_size - Measures the size of a binary tree
- * @tree: Pointer to the root node of the tree to measure the size of
+ * binary_tree_size - measures the size of a binary tree
  *
- * Return: Size of the binary tree, or 0 if tree is NULL
+ * @tree: pointer to the root node of the tree to measure the size
+ *
+ * Return: size of the tree
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
     if (tree == NULL)
         return (0);
 
-    // Recursively count the size of the left subtree
-    size_t left_size = binary_tree_size(tree->left);
-
-    // Recursively count the size of the right subtree
-    size_t right_size = binary_tree_size(tree->right);
-
-    // Return the total size of the tree (1 for the current node + sizes of left and right subtrees)
-    return (1 + left_size + right_size);
+    return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
 }
