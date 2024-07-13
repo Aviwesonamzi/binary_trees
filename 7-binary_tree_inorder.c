a553d357d2914c48e19fbd7d16c9a1ba5f5cb481
root <you@example.com>
2024-07-13 05:55:32 -0700

    binery

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -1,23 +1,17 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_inorder - Goes through a binary tree using in-order traversal
- * @tree: Pointer to the root node of the tree to traverse
- * @func: Pointer to a function to call for each node
+ * binary_tree_inorder - goes through a binary tree using in-order traversal
  *
- * Description: If tree or func is NULL, do nothing
+ * @tree: pointer to the root node of the tree to traverse
+ * @func: pointer to a function to call for each node
  */
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
     if (tree == NULL || func == NULL)
         return;
 
-    // Traverse the left subtree
     binary_tree_inorder(tree->left, func);
-
-    // Call the function for the current node
     func(tree->n);
-
-    // Traverse the right subtree
     binary_tree_inorder(tree->right, func);
 }
